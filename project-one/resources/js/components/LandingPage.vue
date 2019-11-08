e6104449b3712b9a25c986014351e75f7cdb74a3
yoming26 <57481702+yoming26@users.noreply.github.com>
2019-11-08 14:22:24 +0800

    services,register,login

--- project-one/resources/js/components/LandingPage.vue
+++ project-one/resources/js/components/LandingPage.vue
@@ -1,5 +1,22 @@
 <template>
-    <v-container>
-        <h1>landing page</h1>
-    </v-container>
+  <div id="app">
+    <services/>
+  </div>
 </template>
+
+<style>
+
+</style>
+
+<script>
+import services from './Services.vue'
+import register from './Register.vue'
+import login from './Login.vue'
+export default {
+  components: {
+    services,
+    register,
+    login,
+  },
+}
+</script>
