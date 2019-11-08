ba30c2e343dfdbc33cfe107241cfe11e086a6290
Allan Adan <allanadan1999@gmail.com>
2019-11-08 16:53:03 +0800

    bot.vue

---
+++ project-one/resources/js/components/bot.vue
@@ -0,0 +1,12 @@
+<template>
+    <v-content>
+        <script>var BotStar={appId:"sa2152b59-e343-4fbd-a1d9-40c618ea1985",mode:"livechat"};!function(t,a){var e=function(){(e.q=e.q||[]).push(arguments)};e.q=e.q||[],t.BotStarApi=e;!function(){var t=a.createElement("script");t.type="text/javascript",t.async=1,t.src="https://widget.botstar.com/static/js/widget.js";var e=a.getElementsByTagName("script")[0];e.parentNode.insertBefore(t,e)}();}(window,document)</script>
+    </v-content>
+</template
+
+<script>
+export default {
+    
+}
+</script>
+
