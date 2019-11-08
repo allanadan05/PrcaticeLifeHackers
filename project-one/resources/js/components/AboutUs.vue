2177b2786db5de3e2bfea6ce5b6f0c49dff45f00
Francis21419 <francisperlas1998@gmail.com>
2019-11-07 22:48:49 -0800

    AboutUs
    added about us

---
+++ project-one/resources/js/components/AboutUs.vue
@@ -0,0 +1,47 @@
+<template>
+    <v-container>
+        <body>
+            <div class="container">     
+                    <div>
+                        <h1>
+                        ABOUT us
+                        </h1>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur repellat sunt expedita fugit eligendi neque accusamus aliquam hic nihil praesentium? Consequatur, cupiditate. Delectus inventore ab deserunt iste ad totam accusamus.
+                        </p>
+                    </div>
+                    <button id="message">Message Us</button>
+            </div>
+        </body>
+    </v-container>
+</template>
+
+<style scope>
+
+h1{
+    color: black;
+    margin-top: 15%;
+    font-weight:1000;
+    font-family:segoe ui;
+    text-transform: uppercase;
+    letter-spacing: 5px;
+    text-align: center;
+}
+p {
+    display: inline-block;
+    color:black;
+    margin-top:5%;
+    font-size:20px;
+    letter-spacing: 3px;
+    text-align: justify;
+}
+#message{
+    border-color: orangered;
+    background:orangered;
+    color: white;
+    font-size: 2em;
+    width:30%;
+    margin-top:10%;
+    margin-left:35%;
+}
+</style>
